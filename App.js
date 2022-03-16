fe811ea4ea18e09934d01fa00ad0c376709837e5
Avwaveaf <avwave12@gmail.com>
2022-03-17 03:13:03 +0700

    configuring the statusbar height to make content properly justified on IOS and Android

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Platform,
+  StatusBar
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.searchBar}>
+          <Text>Search View</Text>
+        </View>
+        <View style={styles.listView}>
+          <Text>Search View</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style='auto' />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
   },
+  searchBar: {
+    backgroundColor: "red",
+    padding: 16
+  },
+  listView: {
+    flex: 1,
+    backgroundColor: "green",
+    padding: 16
+  }
 });
